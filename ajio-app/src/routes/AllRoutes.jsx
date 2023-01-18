839042f4271c5622ea4b1c83807e160ccf90a496
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-01-18 20:40:41 +0530

    updated

--- ajio-app/src/routes/AllRoutes.jsx
+++ ajio-app/src/routes/AllRoutes.jsx
@@ -3,14 +3,17 @@ import {Routes,Route} from 'react-router-dom';
 import Women from './Women';
 import MenPage from './Men';
 import Navbar from './Navbar';
+import {Box} from '@chakra-ui/react'
 const AllRoutes = () => {
   return (
     <div>
       <Navbar/>
+        <Box marginTop={'90px'} >
         <Routes>
             <Route path='/women' element={<Women />} />
             <Route path='/men' element={<MenPage />} />
         </Routes>
+        </Box>
       
     </div>
   )
